9b1a0302609db1e3905d0b6ced5937fa07ae3a48
ch il <792534005@qq.com>
2018-08-16 17:43:24 +0800

    注册登录框

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-      editingName:false,  
-      resume:{
-          name:'chil',
-          gender: 'girl',
-          birthday: '97.1',
-          job: '前端开发',
-          phone: '15700000000',
-          email: 'example@e.com'
-      }
+        loginvisible :false,
+        signupvisible:false,
+        editingName: false,
+        resume: {
+            name: 'chil',
+            gender: 'girl',
+            birthday: '97.1',
+            job: '前端开发',
+            phone: '15700000000',
+            email: 'example@e.com'
+        }
     },
-    methods:{
-       onedit(key,value){
-           this.resume[key] = value
-       }
-    }
-  })
+    methods: {
+        onedit(key, value) {
+            this.resume[key] = value
+        },
+        clicksave() {
+            var currentUser = AV.User.current();
+            if(!currentUser){
+                this.loginvisible= true
+            }else{
+                this.save()
+            } 
+        },
+        save(){}
+    },
+})
